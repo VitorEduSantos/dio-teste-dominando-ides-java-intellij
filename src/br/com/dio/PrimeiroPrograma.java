4bb7799bf934217a902f1ad423e87861f9dd7026
Vitor Eduardo Silva Santos <vitore.santos@hotmail.com>
2023-03-09 11:42:08 -0300

    criando class livros

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,7 +9,45 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
-        
+        Livros livro1 = new Livros( "O homem do Castelo Alto", 342);
+        System.out.println(livro1);
     }
 
 }
+
+class Livros {
+    private String nome;
+    private Integer npag;
+
+    public Livros(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livros{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
+    }
+}
+
+
